8f78b8a37d4fa11df9ee83d16fb1e22232d57e7d
sahri516 <saghriktk5@gmail.com>
2025-01-02 16:28:06 +0500

    code inserted in proj-email

--- proj-email.py
+++ proj-email.py
@@ -1,13 +1,40 @@
-#This is my email chec
+#This is my email check code 
 
-import smtplib as s
-ob=s.SMTP('smtp.gmail.com',587)
-ob.ehlo()
-ob.starttls()
-ob.login('saghriktk5@gmail.com','57486470SAG')
-subject="python"
-body='hello'
-massage='subject:{}\n\n{}'.format(subject,body)
-listadd=("ktksiraj925@gmail.com")
-ob.sendmail('saghriktk8@gmail.com',listadd,massage)
-print("done")
+while True:
+
+    k,j,d=0,0,0
+    email=input('Enter your email :')
+    if len(email)>=6:
+        if email[0].isalpha():
+            if ('@' in email) and (email.count('@')==1):
+                if (email[-4]=='.'):
+                    for i in email:
+                        if i==i.isspace():
+                            k=1
+                        elif i.isalpha():
+                            if i==i.upper():
+                                j=1
+                        elif i.isdigit():
+                            continue
+                        elif i=='_' or i=='.' or i==('@'): 
+                            continue  
+                        else:
+                            d=1
+
+                    if (k==1) or (j==1) or (d==1):
+                        print('wrong email 5')  
+                    else:
+                        print('right email')          
+                else:
+                    print('wrong email 4')
+            else:
+                print('wrong email 3')
+        else:
+            print('wrong email 2')
+    else:
+        print('wrong email 1')
+
+    if email=="quit":
+        break
+import time
+time.sleep(4)    
